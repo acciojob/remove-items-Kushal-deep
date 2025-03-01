9d6c3deae6509d7aa29b618248f49f9db9dca6e0
Kushal-deep <kushaldeep448@gmail.com>
2025-03-01 11:20:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.getElementsByTagName("input")[0].addEventListener("click",function(){
+	const x=document.getElementById("colorSelect")
+if(x.selectedIndex!==-1){
+	x.remove(x.selectedIndex);
+}
+})
